830829eb1144db1c9e45f01da68ee21240318ef6
HARUKA <haruka.music.style@icloud.com>
2024-12-17 22:19:48 +0900

    base

--- gib/app/page/MainPage/Main.tsx
+++ gib/app/page/MainPage/Main.tsx
@@ -2,11 +2,16 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div>
-      <section>
-        <h1>wellcom to site</h1>
-      </section>
-    </div>
+    <>
+      <div className="image"></div>
+      <article>
+        <div className="image"></div>
+        <p>
+          当社はデザイナーによる完全受注生産の為、１点１点手作業で製作いたします。
+          <br></br>by GOSHI INOUE
+        </p>
+      </article>
+    </>
   );
 };
 
