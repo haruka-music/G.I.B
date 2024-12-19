aa34d59822ec751a179cf1b17fd2668b05cc9105
HARUKA <haruka.music.style@icloud.com>
2024-12-19 22:27:58 +0900

    side

--- app/page/HomePage/MainPage/main.tsx
+++ app/page/HomePage/MainPage/main.tsx
@@ -1,23 +1,61 @@
 import React from "react";
 import Image from "next/image";
 import classes from "@/app/page/HomePage/MainPage/main.module.css";
-// import pick from "@/public/CollectionImag/";
+import pick from "@/public/CollectionImag/pick.jpg";
 
 const main = () => {
   return (
     <>
-      <h1 className={classes.PostHeadline}>コレクション</h1>
       <section className={classes.post}>
         <ul className={classes.ul}>
           <li className={classes.list}>
-            <figure>
-              {/* <Image
+            <figure className={classes.figure}>
+              <Image
                 className={classes.image}
                 src={pick}
                 width={300}
                 height={200}
                 alt="image"
-              /> */}
+              />
+              <figcaption className={classes.title}>
+                <h2>ゴシックピック・ネックレス</h2>
+                <p>2,000</p>
+              </figcaption>
+            </figure>
+            <figure className={classes.figure}>
+              <Image
+                className={classes.image}
+                src={pick}
+                width={300}
+                height={200}
+                alt="image"
+              />
+              <figcaption className={classes.title}>
+                <h2>ゴシックピック・ネックレス</h2>
+                <p>2,000</p>
+              </figcaption>
+            </figure>
+            <figure className={classes.figure}>
+              <Image
+                className={classes.image}
+                src={pick}
+                width={300}
+                height={200}
+                alt="image"
+              />
+              <figcaption className={classes.title}>
+                <h2>ゴシックピック・ネックレス</h2>
+                <p>2,000</p>
+              </figcaption>
+            </figure>
+            <figure className={classes.figure}>
+              <Image
+                className={classes.image}
+                src={pick}
+                width={300}
+                height={200}
+                alt="image"
+              />
               <figcaption className={classes.title}>
                 <h2>ゴシックピック・ネックレス</h2>
                 <p>2,000</p>
